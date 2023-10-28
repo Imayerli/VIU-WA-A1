53b714e064de26da8a2eacaa926024e7478751f6
Imayerli <mayialba21@gmail.com>
2023-10-28 17:18:39 -0500

    navegacion

--- GreatFitness/app/src/main/java/com/hgf/greatfitness/HomeActivity.kt
+++ GreatFitness/app/src/main/java/com/hgf/greatfitness/HomeActivity.kt
@@ -1,25 +1,47 @@
 package com.hgf.greatfitness
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.KeyEvent
-import android.view.View
+import android.util.Log
+import android.widget.Button
+import com.hgf.greatfitness.dietaApp.DietaActivity
+import com.hgf.greatfitness.ejerciciosApp.EjercicioActivity
+import com.hgf.greatfitness.RutinasApp.RutinasActivity
+import com.hgf.greatfitness.saludApp.SaludActivity
 
-class HomeActivity : AppCompatActivity(), View.OnClickListener, View.OnFocusChangeListener, View.OnKeyListener{
+class HomeActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-    }
 
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
-    }
+        //Capturamos botones y lanzamos
+        val btnSalud= findViewById<Button>(R.id.btnSalud)
+        val btnDieta= findViewById<Button>(R.id.btnDieta)
+        val btnEjercicios= findViewById<Button>(R.id.btnEjercicios)
+        val btnVideoRutinas= findViewById<Button>(R.id.btnVideoRutinas)
 
-    override fun onFocusChange(p0: View?, p1: Boolean) {
-        TODO("Not yet implemented")
-    }
 
-    override fun onKey(p0: View?, p1: Int, p2: KeyEvent?): Boolean {
-        TODO("Not yet implemented")
+        //Eventos
+        btnSalud.setOnClickListener {
+            Log.i("MENU --------- ","LANZAMOS APP IMC")
+            navegarHaciaApp(SaludActivity::class.java)
+        }
+        btnDieta.setOnClickListener {
+            Log.i("MENU --------- ","LANZAMOS SALUDA APP")
+            navegarHaciaApp(DietaActivity::class.java)
+        }
+        btnEjercicios.setOnClickListener {
+            Log.i("MENU --------- ","LANZAMOS IMC MEJORADO")
+            navegarHaciaApp(EjercicioActivity::class.java)
+        }
+        btnVideoRutinas.setOnClickListener {
+            Log.i("MENU --------- ","LANZAMOS CANALES APP")
+            navegarHaciaApp(RutinasActivity::class.java)
+        }
+    }
+    fun navegarHaciaApp(clase:Class<*>){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
---
+++ GreatFitness/app/src/main/java/com/hgf/greatfitness/dietaApp/DietaActivity.kt
@@ -0,0 +1,27 @@
+package com.hgf.greatfitness.dietaApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.hgf.greatfitness.R
+
+class DietaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dieta)
+
+        //Primero recuperar es el boton
+        val btnPulsar = findViewById<Button>(R.id.btnPulsar)
+        val edtTexto = findViewById<EditText>(R.id.edtTexto)
+
+        btnPulsar.setOnClickListener {
+            Log.i("RepositorioCompartido","------ BOTON ${btnPulsar.id} PULSADO ------")
+            //val intent = Intent(this, RecibeSaludoActivity::class.java)
+            //putExtra incluyendo la informacion que queramos pasar
+            //intent.putExtra("texto",edtTexto.text.toString())
+           // startActivity(intent)
+        }
+    }
+}
