2eb3dbeea97c747df6c8a0c1acb9b9f49f37c38c
ozz <f.martinezdt@gmail.com>
2023-11-01 20:31:20 -0500

    Se añade recyclerView para la vista de Rutinas

--- GreatFitness/app/src/main/java/com/hgf/greatfitness/dietaApp/DietaActivity.kt
+++ GreatFitness/app/src/main/java/com/hgf/greatfitness/dietaApp/DietaActivity.kt
@@ -1,27 +1,67 @@
 package com.hgf.greatfitness.dietaApp
 
+import GeneralItemAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hgf.greatfitness.R
+import com.hgf.greatfitness.data.Api
+import com.hgf.greatfitness.data.GeneralidadResponse
+import com.hgf.greatfitness.databinding.ActivityDietaBinding
+import com.hgf.greatfitness.databinding.ActivityRutinasBinding
+import retrofit2.Call
+import retrofit2.Response
 
 class DietaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDietaBinding
+    private lateinit var generalItemAdapter: GeneralItemAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dieta)
+        binding = ActivityDietaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //Primero recuperar es el boton
-        val btnPulsar = findViewById<Button>(R.id.btnPulsar)
-        val edtTexto = findViewById<EditText>(R.id.edtTexto)
+        setUpRecyclerView()
+        cargarDatos()
+    }
 
-        btnPulsar.setOnClickListener {
-            Log.i("RepositorioCompartido","------ BOTON ${btnPulsar.id} PULSADO ------")
-            //val intent = Intent(this, RecibeSaludoActivity::class.java)
-            //putExtra incluyendo la informacion que queramos pasar
-            //intent.putExtra("texto",edtTexto.text.toString())
-           // startActivity(intent)
+    private fun setUpRecyclerView() {
+        generalItemAdapter = GeneralItemAdapter(listOf())
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@DietaActivity)
+            adapter = generalItemAdapter
         }
+        Log.d("RutinasActivity", "RecyclerView configurado") // Log para RecyclerView
+    }
+
+    private fun cargarDatos() {
+        val request = Api.build().obtenerGeneralidad("diet")
+
+        request.enqueue(object: retrofit2.Callback<List<GeneralidadResponse>> {
+            override fun onResponse(
+                call: Call<List<GeneralidadResponse>>,
+                response: Response<List<GeneralidadResponse>>
+            ) {
+                Log.d("SaludActivity", "Respuesta API: ${response.body()}") // Log para la respuesta de la API
+
+                if (response.isSuccessful) {
+                    val listaGeneralidad = response.body()
+                    if (listaGeneralidad != null) {
+                        generalItemAdapter.items = listaGeneralidad
+                        generalItemAdapter.notifyDataSetChanged()
+                    }
+                } else {
+                    // Manejo de error
+                }
+            }
+
+            override fun onFailure(call: Call<List<GeneralidadResponse>>, t: Throwable) {
+                // Manejo de fallo en la llamada
+                Log.d("SaludActivity", "Respuesta API error: ${t.message}}") // Log para la respuesta de la API
+
+            }
+        })
     }
 }
